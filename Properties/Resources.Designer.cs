436b29225e28cb1257a285836c13e034474e646b
aryareal <aryadwiputra193@gmail.com>
2025-11-13 20:31:54 +0700

    style: update project name and structure

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YtDownloader.Properties {
+namespace YouTubeDownloader.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace YtDownloader.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YtDownloader.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YouTubeDownloader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
